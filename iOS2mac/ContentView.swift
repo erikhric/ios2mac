a243a2e9f39aa5724eff7f17b39276f47360448a
Erik Hric <erik.hric@senacor.com>
2021-04-15 08:42:33 +0200

    Initial Commit

---
+++ iOS2mac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS2mac
+//
+//  Created by Hric, Erik on 15.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS2mac/iOS2macApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS2macApp.swift
+//  iOS2mac
+//
+//  Created by Hric, Erik on 15.04.21.
+//
+
+import SwiftUI
+
+@main
+struct iOS2macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
